c315383dc9e5bd6f0a81ea940be0fbfe446829f8
Jaden505 <53235649+Jaden505@users.noreply.github.com>
2020-01-30 14:37:04 +0100

    Add files via upload
    Added bigjump

--- javascript.js
+++ javascript.js
@@ -1,50 +1,127 @@
-let jump = false
-let dinosaur = document.getElementById('dinosaur')
-let jumpheight = 400
-let floor = document.getElementById('floor')
-let floorpos = 360
-let floorwidth = 600
-let speedmove = 1
-let x = true
-let start = false
+let list_random = []
+let number_index = 0
+let number_index2 = 0
+let number = 10
+let i;
+let list_heights = []
+let list = []
+
+let status = document.getElementById('status');
+let buttonS = document.getElementById('Sort');
+let buttonR = document.getElementById('Randomize');
+
+buttonS.disabled = "disabled"
+buttonR.disabled = ""
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-async function moveSide() {
-  if (start == false) {
-    start = true
-  while (x) {
-    floorpos -= 1
-    floor.style.left = floorpos + 'px'
-    floor.style.clip = 'rect(0px,600px,200px,0px)'
-    await sleep(1)
-  }
-}
-}
-
-async function smallJump(e) {
-  let spacebar = (e.keyCode ? e.keyCode : e.which)
-  if (spacebar == 32 && jump == false) {
-      jump = true
-      for (let i = 0; i < 20; i++) {
-        jumpheight -= 5
-        dinosaur.style.top = jumpheight + 'px'
-        await sleep(8)
-      }
-      await sleep(10)
-      for (let i = 0; i < 20; i++) {
-        jumpheight += 5
-        dinosaur.style.top = jumpheight + 'px'
-        await sleep(8)
-      }
-      jump = false
+function AddNumbers() {
+  for (i = 10; i < 110; i++) {
+    list_random.push(i);
   }
 }
 
-window.addEventListener("keydown", function (e) {
-  if (e.code == 'Space') {
-    console.log(e.code)
+let x = document.querySelectorAll("#rectup");
+let y = document.querySelectorAll("#rectdown");
+
+function MyFunc() {
+  buttonS.disabled = ""
+
+  document.getElementById('status').innerHTML = 'Randomized'
+  AddNumbers()
+  for (const rctx of x) {
+    let random_nr = list_random[Math.floor(Math.random() * list_random.length)];
+    rctx.style.height = random_nr
+    list_random.splice(list_random.indexOf(random_nr), 1);
   }
-})
+}
+
+async function GetLowest() {
+  number_index3 = 0
+  number = 10
+  list = []
+
+  document.getElementById('status').innerHTML = 'Sorting';
+  for (i of y) {
+    list.push(i)
+  }
+
+  let z;
+  for (z = 0; z < 100; z++) {
+  for (rctx of x) {
+      if (rctx.style.height == number + 'px') {
+        console.log(rctx)
+
+        let index_lowest = list_heights.indexOf(rctx.style.height);
+        let first_lowest = x[number_index3];
+        let first_lowest_index = x[number_index3].style.height;
+        let smallest_number = rctx.style.height;
+
+        console.log(index_lowest)
+        console.log(first_lowest)
+        console.log(smallest_number)
+
+        let first_nr = first_lowest.style.height
+        console.log(first_nr)
+        first_lowest.style.height = smallest_number
+        rctx.style.height = first_nr
+
+        for (rcty of list) {
+          document.getElementById('status').innerHTML = 'Analyzing';
+          rcty.style = 'fill:rgb(255,0,0);stroke-width:1;stroke:rgb(0,0,0)';
+          await sleep(1);
+          rcty.style = 'fill:rgb(255,255,255);stroke-width:1;stroke:rgb(0,0,0)';
+        }
+        list.shift()
+
+        number += 1
+        number_index3 += 1
+      }
+  }
+}
+  ChangeColorGreen()
+  document.getElementById('status').innerHTML = 'Sorted';
+
+}
+
+function NewFunc() {
+  number_index = 0
+  list_heights = []
+  for (element of x) {
+    let element1_h = x[number_index];
+    list_heights.push(element.style.height);
+
+    number_index += 1
+  }
+  GetLowest();
+}
+
+async function ChangeColorRed() {
+    // Red
+    buttonR.disabled = "disabled"
+    buttonS.disabled = "disabled"
+
+    for (const rcty of y) {
+      document.getElementById('status').innerHTML = 'Analyzing';
+      rcty.style = 'fill:rgb(255,0,0);stroke-width:1;stroke:rgb(0,0,0)';
+      await sleep(10);
+    }
+    for (const rcty of y) {
+      rcty.style = 'fill:rgb(255,255,255);stroke-width:1;stroke:rgb(0,0,0)';
+    }
+    NewFunc();
+  }
+
+async function ChangeColorGreen() {
+    // Green
+    for (const rcty of y) {
+      rcty.style = 'fill:rgb(0,255,0);stroke-width:1;stroke:rgb(0,0,0)';
+      await sleep(10);
+    }
+    for (const rcty of y) {
+      rcty.style = 'fill:rgb(255,255,255);stroke-width:1;stroke:rgb(0,0,0)';
+    }
+    buttonR.disabled = ""
+  }
