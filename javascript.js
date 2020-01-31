debf1129c8fd59aaa244a9a9d9922d60d38c7b0f
Jaden505 <53235649+Jaden505@users.noreply.github.com>
2020-01-31 11:52:12 +0100

    Add files via upload
    Floor move working

--- javascript.js
+++ javascript.js
@@ -1,31 +1,69 @@
 let jump = false
 let dinosaur = document.getElementById('dinosaur')
 let jumpheight = 400
+
 let floor = document.getElementById('floor')
 let floorpos = 360
-let floorwidth = 600
 let backlen = 0
+
+let floor2 = document.getElementById('floor2')
+let floorpos2 = 960
+let frontlen = 0
+
 let speedmove = 1
-let x = true
 let start = false
-let keydown = true
-let keydown2 = true
+let x = true
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function moveSide() {
+  // Loop floor
   if (start == false) {
     start = true
-  while (x) {
-    floorpos -= 1
-    backlen += 1
-    floor.style.left = floorpos + 'px'
-    floor.style.clip = 'rect(0px,600px,200px,' + backlen + 'px)'
-    if (backlen >= 600) { floor.style.display = 'none' }
-    await sleep(1)
-  }
+    while (x) {
+      while (frontlen <= 600) {
+        // Move line 1 to left and keep in same place
+        floorpos -= speedmove
+        backlen += speedmove
+        floor.style.left = floorpos + 'px'
+        floor.style.clip = 'rect(0px,600px,200px,' + backlen + 'px)'
+
+        // Move line 2 to left and keep in same place
+        floorpos2 -= speedmove
+        frontlen += speedmove
+        floor2.style.clip = 'rect(0px,' + frontlen + 'px,200px,0px)'
+        floor2.style.left = floorpos2 + 'px'
+
+        await sleep(1)
+      }
+
+      floorpos = 960
+      backlen = 0
+      floorpos2 = 360
+      frontlen = 0
+
+      while (backlen <= 600) {
+        // Repeat but line 1 and 2 switched position
+          floorpos -= speedmove
+          backlen += speedmove
+          floor.style.left = floorpos + 'px'
+          floor.style.clip = 'rect(0px,' + backlen + 'px,200px,0px)'
+
+          floorpos2 -= speedmove
+          frontlen += speedmove
+          floor2.style.left = floorpos2 + 'px'
+          floor2.style.clip = 'rect(0px,600px,200px,' + frontlen + 'px)'
+
+          await sleep(1)
+      }
+      floorpos = 360
+      backlen = 0
+      floorpos2 = 960
+      frontlen = 0
+      speedmove += 0.2
+    }
   }
 }
 
@@ -38,25 +76,6 @@ async function smallJump(e) {
         dinosaur.style.top = jumpheight + 'px'
         await sleep(8)
       }
-
-      keydown = true
-      keydown2 = true
-
-      // Check if key up
-      document.addEventListener('keyup', async function (event) {
-        if (event.code == 'Space' && keydown) {
-          keydown2 = false
-          keydown = false
-          console.log(event.code + 'bigjump')
-        }})
-
-      document.addEventListener('keydown', async function (event) {
-        if (event.code == 'Space' && keydown2) {
-          keydown = false
-          keydown2 = false
-          console.log(event.code + 'smalljump')
-        }})
-
       await sleep(10)
       for (let i = 0; i < 20; i++) {
         jumpheight += 5
